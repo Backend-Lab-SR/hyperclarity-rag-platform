9641cf2d5fd226fe5025e9bb1d9adf05f6c9583f
Sangeetha <backendlab@users.noreply.github.com>
2026-05-21 16:50:28 -0400

    feat: initialize spring boot backend

---
+++ backend/src/main/java/com/hyperclarity/HyperClarityApplication.java
@@ -0,0 +1,12 @@
+package com.hyperclarity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperClarityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyperClarityApplication.class, args);
+    }
+}
